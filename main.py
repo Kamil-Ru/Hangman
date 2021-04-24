e4ca1acd00b71bcd968c9574832f769bdcf6afac
Kamil Ruchaj <kamilruchaj@gmail.com>
2021-04-24 21:55:12 +0200

    completed exercise

--- main.py
+++ main.py
@@ -1,64 +1,9 @@
 import random
+from replit import clear 
+from hangman_art import stages, logo
+from hangman_words import word_list
 
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
-
-
-word_list = ["aardvark", "baboon", "camel"]
+print(logo)
 chosen_word = random.choice(word_list)
 lives = 6
 
@@ -71,19 +16,28 @@ display = ["_" for _ in range(len(chosen_word))]
 print(display)
 
 game_on = True
+guesses_letters = ''
 
 while game_on:
     
     guess = input("Guess a letter: \n").lower()
+    clear()
+    #Printing ifromation if letter has alredy chosen
+    if guess in guesses_letters:
+        print(f"Letter {guess} has already chosen")
     
     #Check guessed letter
     for ID_letter in range(len(chosen_word)):
-        
+
         if chosen_word[ID_letter] == guess:
             display[ID_letter] = guess
-
+    
+    #Adding gueses leetter to string with the letters
+    guesses_letters += guess
+            
     #Check if user is wrong.
     if guess not in chosen_word:
+        print(f'Letter "{guess}" is not in the word.')
         lives -= 1
         if lives == 0:
             game_on = False
