4d725eff8072031ab2a18b70c15c75e0573eb860
Kamil Ruchaj <kamilruchaj@gmail.com>
2021-04-24 21:22:20 +0200

    Working

--- main.py
+++ main.py
@@ -1,30 +1,103 @@
-#Step 1 
 import random
 
-word_list = ["aardvark", "baboon", "camel"]
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
 
-# 1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+
+word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
+lives = 6
 
 #Testing code
 print(f'Pssst, the solution is {chosen_word}.')
+
+#Create blanks
 display = ["_" for _ in range(len(chosen_word))]
+#Displey blanks
 print(display)
 
-# 2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-guess = input("Guess a letter: \n").lower()
+game_on = True
 
-
-# 3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-
-for ID_letter in range(len(chosen_word)):
+while game_on:
     
-    if chosen_word[ID_letter] == guess:
-        display[ID_letter] = guess
-        print(True)
-    else:
-        print(False)
+    guess = input("Guess a letter: \n").lower()
+    
+    #Check guessed letter
+    for ID_letter in range(len(chosen_word)):
+        
+        if chosen_word[ID_letter] == guess:
+            display[ID_letter] = guess
+
+    #Check if user is wrong.
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            game_on = False
+            print('GAME OVER')
+    
+    #Check if user has got all letters.            
+    elif '_' not in display:
+        game_on = False
+        print('YOU WIN')
+            
+    #Join all the elements in the list and turn it into a String.        
+    print(f"{' '.join(display)}")   
+    
+    print(stages[lives])
     
         
-
 print(display)
